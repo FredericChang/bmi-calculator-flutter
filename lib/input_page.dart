029337d94fefb7e74917d445879e2a3089caf3eb
Frederic <paulpoiu123@gmail.com>
2022-02-19 23:02:38 +0100

    Updated

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,6 +5,7 @@ import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const Color colorNumber = Color(0xFF1D1E33);
+const inactivateColour = Color(0xFF111328);
 const Color bottomColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -13,6 +14,27 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleCardColour = inactivateColour;
+  Color femaleCardColour = inactivateColour;
+
+  void updatedColour(int gender) {
+    if (gender == 1) {
+      if (femaleCardColour == inactivateColour) {
+        femaleCardColour = colorNumber;
+      } else {
+        femaleCardColour = inactivateColour;
+      }
+    }
+    if (gender == 0 ) {
+      if (maleCardColour == inactivateColour) {
+        maleCardColour = colorNumber;
+      } else {
+        maleCardColour = inactivateColour;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +47,31 @@ class _InputPageState extends State<InputPage> {
             Expanded(child: Row(
               children: <Widget>[
                 Expanded(
-                    child: ReusableCard(
-                      colour : colorNumber,
-                      cardChild: cardChildWidget(icon: FontAwesomeIcons.mars, colour: Colors.pink, textName: 'Female',),
+                    child: GestureDetector(
+                      onTap: (){
+                        print('Female');
+                        setState(() {
+                          updatedColour(1);
+                        });
+                      },
+                      child: ReusableCard(
+                        colour : femaleCardColour,
+                        cardChild: cardChildWidget(icon: FontAwesomeIcons.mars, colour: Colors.pink, textName: 'Female',),
+                      ),
                     )
                 ),
                 Expanded(
-                    child: ReusableCard(
-                      colour : colorNumber,
-                      cardChild: cardChildWidget(icon: FontAwesomeIcons.facebook, colour: Colors.pink, textName: 'Male',),
+                    child: GestureDetector(
+                      onTap: (){
+                        print('Male');
+                        setState(() {
+                          updatedColour(0);
+                        });
+                      },
+                      child: ReusableCard(
+                        colour : maleCardColour,
+                        cardChild: cardChildWidget(icon: FontAwesomeIcons.facebook, colour: Colors.pink, textName: 'Male',),
+                      ),
                     )
                 ),
               ],
